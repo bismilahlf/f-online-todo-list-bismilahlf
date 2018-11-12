1216e20a482ea99b2503e0842fb9888d73684166
bismilahlf <bismilah.lf@gmail.com>
2018-11-12 19:03:53 +0100

    Add modal and finished tasks functionality

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
 'use strict';
 
-var newTaskButton = document.querySelector('.modal__add-button');
-var taskList = document.querySelector('.task-list');
+var triggerModalButton = document.querySelector('.modal__trigger-button');
+var modal = document.querySelector('.modal');
+var modalBackground = document.querySelector('.modal__background');
+var newTaskButton = document.querySelector('.modal__add-task-button');
+var taskListCurrent = document.querySelector('.task-list__current');
+var taskListCompleted = document.querySelector('.task-list__completed');
 var taskName = document.querySelector('.modal__input');
 var newListElement;
 var newTask;
 
+// ABRE/CIERRA EL MODAL
+function toggleModal() {
+  modal.classList.toggle('modal-open');
+}
+
+triggerModalButton.addEventListener('click', toggleModal);
+modalBackground.addEventListener('click', toggleModal);
+
+
+// AÑADE UNA TAREA A LA LISTA
 newTaskButton.addEventListener('click', addNewTask);
 
 function addNewTask() {
   newListElement = document.createElement('li');
   newTask = document.createTextNode(taskName.value);
-  newListElement.append(newTask);
-  newListElement.addEventListener('click', completeTask);
-  taskList.appendChild(newListElement);
+  newListElement.appendChild(newTask);
+  newListElement.addEventListener('click', toggleTask);
+  taskListCurrent.appendChild(newListElement);
+
+  toggleModal();
 }
 
-function completeTask(e) {
-  e.target.classList.toggle('completed-task');
-  
+// MARCA/DESMARCA UNA TAREA COMO COMPLETADA 
+function toggleTask(e) {
+  if(e.target.classList.contains('completed-task')){
+    e.target.classList.remove('completed-task');
+    taskListCompleted.removeChild(e.target);
+    taskListCurrent.appendChild(e.target);
+  } else {
+    e.target.classList.add('completed-task');
+    taskListCurrent.removeChild(e.target);
+    taskListCompleted.appendChild(e.target);
+  }
 }
+
