e946916dc5456156f19f074ec3486b29d7139f22
bismilahlf <bismilah.lf@gmail.com>
2018-11-13 19:42:04 +0100

    Add date functionality

--- main.js
+++ main.js
@@ -10,6 +10,47 @@ var taskName = document.querySelector('.modal__input');
 var newListElement;
 var newTask;
 
+
+// OBTIENE LA FECHA DE HOY
+var date = new Date();
+var currentMonthDay = date.getDate();
+var monthDayTag = document.querySelector('.date-number');
+monthDayTag.innerHTML = currentMonthDay;
+
+var days = new Array(7)
+days[0] = "Domingo";
+days[1] = "Lunes";
+days[2] = "Martes";
+days[3] = "Miércoles";
+days[4] = "Jueves";
+days[5] = "Viernes";
+days[6] = "Sábado";
+var currentWeekDay = days[date.getDay()];
+var weekDayTag = document.querySelector('.date-day');
+weekDayTag.innerHTML = currentWeekDay;
+
+var months = new Array();
+months[0] = "Enero";
+months[1] = "Febrero";
+months[2] = "Marzo";
+months[3] = "Abril";
+months[4] = "Mayo";
+months[5] = "Junio";
+months[6] = "Julio";
+months[7] = "Agosto";
+months[8] = "Septiembre";
+months[9] = "Octubre";
+months[10] = "Noviembre";
+months[11] = "Diciembre";
+var currentMonth = months[date.getMonth()];
+var monthTag = document.querySelector('.date-month');
+monthTag.innerHTML = currentMonth;
+
+var currentYear = date.getFullYear();
+var yearTag = document.querySelector('.date-year');
+yearTag.innerHTML = currentYear;
+
+
 // ABRE/CIERRA EL MODAL
 function toggleModal() {
   modal.classList.toggle('modal-open');
